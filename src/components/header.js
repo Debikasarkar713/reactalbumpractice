a60574264758124aa07b391b73121e677778c4a5
Debika Sarkar <debikasarkar713@gmail.com>
2017-12-05 18:05:14 -0500

    imports

---
+++ src/components/header.js
@@ -0,0 +1,10 @@
+//Import libraries for making a component
+import React from 'react';
+import { Text } from 'react-native';
+// make a component
+const Header = ()=> {
+  return <Text>Albums</Text>;
+};
+
+// make a component available to other parts of the app
+

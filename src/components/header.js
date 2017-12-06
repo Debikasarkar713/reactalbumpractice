1cc7d23fd2ea32d59a3c307d31a909f65fcaeb85
Debika Sarkar <debikasarkar713@gmail.com>
2017-12-05 21:31:12 -0500

    added font

--- src/components/header.js
+++ src/components/header.js
@@ -3,7 +3,16 @@ import React from 'react';
 import { Text } from 'react-native';
 // make a component
 const Header = ()=> {
-  return <Text>Albums</Text>;
+  const { textStyle } = styles;
+
+  return <Text style={textStyle}>Albums</Text>;
+
+};
+
+const styles = {
+ textStyle: {
+  fontSize: 20
+ }
 };
 
 // make a component available to other parts of the app

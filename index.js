1d3497d727d8d6ea26a088ffe15de3b99fc7c925
Debika Sarkar <debikasarkar713@gmail.com>
2017-12-05 21:22:13 -0500

    added header component

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 // import a library to help create a component
 import React from 'react';
-import { Text, AppRegistry } from 'react-native';
+import { AppRegistry } from 'react-native';
 import Header from './src/components/header'
 // create a component
 const App = () => {
   return (
-<Text>Some Text</Text>
+<Header />
     );
 };
 

395d74787670c5fc396431f0472df21db107205c
Debika Sarkar <debikasarkar713@gmail.com>
2017-12-07 11:04:36 -0500

    fixed package bundle issue

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
 // import a library to help create a component
 import React from 'react';
-import { AppRegistry } from 'react-native';
-import Header from './src/components/header'
+import { AppRegistry, View } from 'react-native';
+import Header from './src/components/Header';
+import AlbumList from './src/components/AlbumList';
 // create a component
 const App = () => {
   return (
+<View>
 <Header headerText={'Albums'}/>
+<AlbumList />
+</View>
     );
 };
 
 //render it to the device
 AppRegistry.registerComponent('albums', () => App );
+
+
+//react-native run-ios is the way to start

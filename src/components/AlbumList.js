922aa064d30ef3e931696ab5d8ec53670ef6e915
Debika Sarkar <debikasarkar713@gmail.com>
2017-12-07 14:47:03 -0500

    npm axios is the worst and that was so stressful just use fetch

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -2,6 +2,14 @@ import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 
 class AlbumList extends Component {
+  componentWillMount(){
+    fetch('https://rallycoding.herokuapp.com/api/music_albums')
+    .then((response) => response.json())
+    .then((responseData) => {
+      console.log(responseData);
+    })
+  }
+
   render() {
   return (
     <View>
